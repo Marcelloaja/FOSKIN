659251b440baa9293e4dfbdad1314e720041c16a
Marcello <marcelloilham54@gmail.com>
2024-11-22 19:02:20 +0700

    update
    splash screen & introduction

---
+++ app/src/main/java/com/project/foskin/ui/introduction/IntroActivity.kt
@@ -0,0 +1,17 @@
+package com.project.foskin.ui.introduction
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.foskin.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_intro)
+        supportActionBar?.hide()
+    }
+}
