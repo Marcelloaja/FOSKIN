4b39ef09a6727e5f26d71e4a96501f3b308ac33a
Marcello <marcelloilham54@gmail.com>
2024-11-22 22:39:23 +0700

    update
    fix it button next and get started

--- app/src/main/java/com/project/foskin/ui/introduction/IntroActivity.kt
+++ app/src/main/java/com/project/foskin/ui/introduction/IntroActivity.kt
@@ -1,17 +1,39 @@
 package com.project.foskin.ui.introduction
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.project.foskin.MainActivity
 import com.project.foskin.R
 
 class IntroActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intro)
         supportActionBar?.hide()
+
+        val motionLayout = findViewById<MotionLayout>(R.id.main)
+
+        val btnNext1 = findViewById<Button>(R.id.btnNext)
+        val btnNext2 = findViewById<Button>(R.id.btnNext2)
+        val btnStarted = findViewById<Button>(R.id.btnStarted)
+
+        btnNext1.setOnClickListener {
+            motionLayout.transitionToEnd()
+        }
+
+        btnNext2.setOnClickListener {
+            motionLayout.transitionToEnd()
+        }
+
+        btnStarted.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
